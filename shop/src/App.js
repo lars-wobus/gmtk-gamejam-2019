f9ae4c974b8b36a760f7ed14d8b0475c19a0f654
Lars Wobus <lars.wobus@web.de>
2019-08-02 23:45:35 +0200

    Show 5 buttons by reading in json

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {EditorView} from './components/editor-view';
-import {ShopView} from './components/shop-view';
+import { EditorView } from './components/editor-view';
+import { ShopView } from './components/shop-view';
+
+import shopData from './data/shop-data';
 
 function App() {
   return (
     <div className="App">
       <EditorView />
-      <ShopView />
+      <ShopView data={shopData}/>
     </div>
   );
 }
--- shop/src/components/shop-view.js
+++ shop/src/components/shop-view.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {Button} from './button';
 
-export const ShopView = () => {
+export const ShopView = ({data={}}) => {
+  
+  const createButtons = (buttons) => {
+    const index = 0;
+    return buttons.map(element => {
+      return <Button
+        label={element.label}
+        key={`${element.label}_${index}`}
+      />
+    });
+  }
+
   return <div id = "shopview">
     Lorem Ipsum Dolor Sit Amet
-    <Button label="Buy Now" />
+    {createButtons(data.buttons)}
   </div>
 };
