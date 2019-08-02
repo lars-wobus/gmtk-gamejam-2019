7537d2f6f06f268c2881e15f2eb147210e70e7cc
Lars Wobus <lars.wobus@web.de>
2019-08-02 23:21:14 +0200

    2 frames with one button for buying

---
+++ shop/src/components/editor-view.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const EditorView = () => {
+  return <div id = "editorview">
+    Lorem Ipsum Dolor Sit Amet
+  </div>
+};
---
+++ shop/src/components/shop-view.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Button} from './button';
+
+export const ShopView = () => {
+  return <div id = "shopview">
+    Lorem Ipsum Dolor Sit Amet
+    <Button />
+  </div>
+};
