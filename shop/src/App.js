56c92662beccd1b55a5c02edbe248a560bd69321
Lars Wobus <lars.wobus@web.de>
2019-08-02 23:50:36 +0200

    Apply logic from shop view in editor view

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import { EditorView } from './components/editor-view';
 import { ShopView } from './components/shop-view';
 
+import editorData from './data/editor-data';
 import shopData from './data/shop-data';
 
 function App() {
   return (
     <div className="App">
-      <EditorView />
+      <EditorView data={editorData}/>
       <ShopView data={shopData}/>
     </div>
   );
--- shop/src/components/editor-view.js
+++ shop/src/components/editor-view.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import {Button} from './button';
 
-export const EditorView = () => {
+export const EditorView = ({data={}}) => {
+  const createButtons = (buttons) => {
+    const index = 0;
+    return buttons.map(element => {
+      if (!element.is_visible) {
+        return null;
+      }
+      return <Button
+        label={element.label}
+        key={`${element.label}_${index}`}
+      />
+    });
+  }
+
   return <div id = "editorview">
     Lorem Ipsum Dolor Sit Amet
+    {createButtons(data.buttons)}
   </div>
 };
