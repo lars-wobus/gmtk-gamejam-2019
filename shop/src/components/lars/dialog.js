d21a16f589b76e5cbf3f128bb7b55747b3ab8f76
Lars Wobus <lars.wobus@web.de>
2019-08-03 14:45:41 +0200

    Default dialog ready + no text selection (globally)

--- shop/src/components/lars/dialog.js
+++ shop/src/components/lars/dialog.js
@@ -3,10 +3,12 @@ import React from 'react';
 export const Dialog = (
   {  }
 ) => {
-  return <div className="background">
+  return <div className="tutorial-background">
     <div
       className="tutorial-dialog"
     >
+      <h1>Headline</h1>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
       <div className="skip-button">Skip tutorial</div>
       <div className="previous-button">Previous</div>
       <div className="next-button">Next</div>
