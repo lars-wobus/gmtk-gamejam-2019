323d05b4a1c39a589b27255fd484562abaf83912
Lars Wobus <lars.wobus@web.de>
2019-08-03 19:26:11 +0200

    Remove css class from button component

--- shop/src/components/button.js
+++ shop/src/components/button.js
@@ -4,7 +4,7 @@ import { noop } from '../utils/noop';
 export const Button = (
   {label = "Buy", onButtonClick = noop}
 ) => {
-  return <div id = "buy" onClick={onButtonClick}>
+  return <div id = "" onClick={onButtonClick}>
     {label}
   </div>
 };
